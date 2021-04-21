8ed2115281ddbb4b05033a3399db568b41aad2e6
Sara Cantero <cantero.mesar20@cadiz.salesianos.edu>
2021-04-21 19:42:22 +0200

    PanelAdmin

--- nucleo/admin.py
+++ nucleo/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from nucleo.models import Cliente, Especialista, cita, mensaje
 
+@admin.register(Especialista)
+class EspecialistaAdmin(admin.ModelAdmin):
+    list_display = ('dni', 'nombre', 'apellidos', 'direccion' , 'biografia')
+    fields = [('dni' , 'idUsuario') , ('apellidos', 'nombre' ),'fechaNacimiento', 'foto' , 'direccion' , 'biografia' ]
 
-admin.site.register(Cliente)
-admin.site.register(Especialista)
-admin.site.register(cita)
-admin.site.register(mensaje)
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('dni', 'nombre', 'apellidos')
+  
 
 
+@admin.register(cita)
+class CitaAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'idCliente', 'idEspecialista', 'informe','realizada')
+    list_filter = ('realizada' , 'fecha')
+    
+
+
+
+
+@admin.register(mensaje)
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ('idEmisor', 'idReceptor', 'fecha','asunto','leido')
+    
 
